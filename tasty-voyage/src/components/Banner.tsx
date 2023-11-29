4491fdf3adc40e8076d17df05542323550a2213a
3nsar <84131267+3nsar@users.noreply.github.com>
2023-11-29 12:25:26 +0100

    added banner

--- tasty-voyage/src/components/Banner.tsx
+++ tasty-voyage/src/components/Banner.tsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import "./Banner.scss"
+// Banner.tsx
+
+import React, { useEffect } from 'react';
+import './Banner.scss';
 
 const Banner = () => {
+
+  const scrollers = document.querySelectorAll(".scroller");
+
+  function addAnimation(){
+    scrollers.forEach((scroller =>{
+      scroller.setAttribute("data-animated", "true")
+    }))
+  }
+
+  if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation()
+  }
+   
+
   return (
     <div className='banner-container'>
-        <p>100% Vegan</p>
-        <p>Plant Friendly</p>
-        <p>Healthy</p>
+      <div className="scroller">
+        <div className="scroller-inner">
+          <p>100% VEGAN</p>
+          <div className='banner-emoji'>&#x1F9C0;</div>
+          <p>PLANT FRIENDLY</p>
+          <div className='banner-emoji'>&#x1F957;</div>
+          <p>HEALTHY</p>
+          <div className='banner-emoji'>&#x1F957;</div>
+          <p>PLANT FRIENDLY</p>
+          <div className='banner-emoji'>&#x1F373;</div>
+          <p>100% VEGAN</p>
+          <div className='banner-emoji'>&#x1F957;</div>
+          <p>PLANT FRIENDLY</p>
+          <div className='banner-emoji'>&#x1F373;</div>
+          <p>100% VEGAN</p>
+          <p>100% VEGAN</p>
+          <div className='banner-emoji'>&#x1F9C0;</div>
+          <p>PLANT FRIENDLY</p>
+          <div className='banner-emoji'>&#x1F957;</div>
+          <p>HEALTHY</p>
+          <div className='banner-emoji'>&#x1F957;</div>
+          <p>PLANT FRIENDLY</p>
+          <div className='banner-emoji'>&#x1F373;</div>
+          <p>100% VEGAN</p>
+          <div className='banner-emoji'>&#x1F957;</div>
+          <p>PLANT FRIENDLY</p>
+          <div className='banner-emoji'>&#x1F373;</div>
+          <p>100% VEGAN</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
