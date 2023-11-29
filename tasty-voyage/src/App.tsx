762977ef417e76c80079c814f1a297ead83fc1ad
3nsar <84131267+3nsar@users.noreply.github.com>
2023-11-29 15:29:10 +0100

    commit

--- tasty-voyage/src/App.tsx
+++ tasty-voyage/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import "./App.css";
-import Hero from './components/Hero';
-import Banner from './components/Banner';
+import Hero from './components/Hero/Hero';
+import Banner from './components/Banner/Banner';
+import { Navbar } from './components/Navbar/Navbar';
+import Info1 from './components/Sections/Info1';
+import Info2 from './components/Sections/Info2';
+import Info3 from './components/Sections/Info3';
 
 const App = () => {
   return (
     <div className='App'>
+        <Navbar/>
         <Hero/>
-        <Banner/>
+        <Info1/>
+        <Info2/>
+        <Info3/>
     </div>
   )
 }
---
+++ tasty-voyage/src/components/Sections/Info3.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Info3.scss"
+
+const Info3 = () => {
+  return (
+    <div className='info3-container'>
+      <div className="info3-content">
+        <p>HALLLO</p>
+       <img src="" alt="pic" />
+      </div>
+    </div>
+  )
+}
+
+export default Info3
