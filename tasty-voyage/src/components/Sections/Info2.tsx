8dbdc9f46a7bd4ac4a1397773c75adadef57e9d9
3nsar <84131267+3nsar@users.noreply.github.com>
2023-11-30 21:15:08 +0100

    commit

--- tasty-voyage/src/components/Sections/Info2.tsx
+++ tasty-voyage/src/components/Sections/Info2.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import "./Info2.scss"
+import people from "../../images/people/people2.svg"
+import { Parallax } from 'react-parallax'
+
 
 const Info2 = () => {
   return (
+  
     <div className='info2-container'>
       <div className="info2-content">
-        <p>HALLLO</p>
-       <img src="" alt="pic" />
+        <div className="info2-text">
+          <h1>INSPIRING GOOD THROUGH TASTY GOODNESS</h1>
+          <p>We’re here to inspire discovery and tempt everyone to eat more plants. Because we care about the future – yours, ours and the planet’s.</p>
+       </div>
+       <img src={people} alt="pic" height="300px"/>
       </div>
     </div>
+
   )
 }
 
--- tasty-voyage/src/components/Sections/Info3.tsx
+++ tasty-voyage/src/components/Sections/Info3.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import "./Info3.scss"
+import people from "../../images/people/people3.svg"
+import { Parallax } from 'react-parallax'
+
 
 const Info3 = () => {
   return (
+  
     <div className='info3-container'>
       <div className="info3-content">
-        <p>HALLLO</p>
-       <img src="" alt="pic" />
+      <img src={people} alt="pic" height="200px"/>
+        <div className="info3-text">
+          <h1>POWERED BY POSITIVITY</h1>
+          <p>We’re not crusading or going up against an industry. <br />We’re for; for flavour, for fun, for what’s possible.</p>
+       </div>
       </div>
     </div>
+
   )
 }
 
