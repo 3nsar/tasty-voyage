80663a651f888fed912b9b289070c829eba15e2d
3nsar <84131267+3nsar@users.noreply.github.com>
2023-11-30 09:26:08 +0100

    navbar update

--- tasty-voyage/src/components/Navbar/Navbar.tsx
+++ tasty-voyage/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.scss';
 import { RiPlantFill } from "react-icons/ri";
+import { GiCook } from "react-icons/gi";
 
 export const Navbar = () => {
   const [rotation, setRotation] = useState(0);
@@ -22,6 +23,12 @@ export const Navbar = () => {
 
   return (
     <div className='navbar-container'>
+      <div className='navbar-content'>
+        <a href="#">ABOUT</a>
+        <a href="#">PRODUCTS</a>
+        <h1>Tasty Voyage <span><GiCook/></span></h1>
+        <button>CONTACT US</button>
+      </div>
       <div className='circle' style={{ transform: `rotate(${rotation}deg)` }}>
         <RiPlantFill />
       </div>
