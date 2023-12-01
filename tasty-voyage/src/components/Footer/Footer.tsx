5fe3a70382deeab3804b99ffebbbe66f0cdc76ae
3nsar <84131267+3nsar@users.noreply.github.com>
2023-12-01 10:51:45 +0100

    commit

--- tasty-voyage/src/components/Footer/Footer.tsx
+++ tasty-voyage/src/components/Footer/Footer.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './Footer.scss'; // Import the hero.scss file
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-        <div className="footer-text">
-        <p>Instagram</p>
-        <p>2023 TASTY VOYAGE</p>
+        <div className="footer-emojis">
+            <ul>
+              <li><FaFacebook/></li>
+              <li><FaInstagram/></li>
+              <li><FaSquareXTwitter/></li>
+            </ul>
         </div>
+         <div className="footer-text">
+            <p>© 2023 TASTY VOYAGE</p>
+         </div>
     </div>
   )
 }
