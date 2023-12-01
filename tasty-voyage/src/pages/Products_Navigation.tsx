32860f4cfc16c32fc74e22ac4571ce4f860597de
3nsar <84131267+3nsar@users.noreply.github.com>
2023-12-01 18:18:56 +0100

    commit

---
+++ tasty-voyage/src/pages/Products_Navigation.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Products_Navigation.scss';
+
+const Products_Navigation = () => {
+  return (
+    <div className='products-container'>
+      <ul>
+        <li>BURGER</li>
+        <li>PIECES</li>
+        <li>MINCE</li>
+        <li>FILLETS</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Products_Navigation
